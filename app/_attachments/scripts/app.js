7dbb6ce77dca2f92382a6edfb28d12df2c603d72
StijnDefour <defourstijn10@gmail.com>
2017-11-08 17:15:57 +0100

    Implement save service

--- app/_attachments/scripts/app.js
+++ app/_attachments/scripts/app.js
@@ -28,6 +28,8 @@ angular.module('parkingApp', ['ngRoute'])
 	    			}
 	    			console.log(arrTitles);
 	    			$scope.films = JSON.stringify(arrTitles);
+	    			
+	    			saveSrv.setObject(acteur, data);
 	    		});
 	    	});
     })
@@ -48,90 +50,6 @@ angular.module('parkingApp', ['ngRoute'])
 	    		};
     })
    
-    /*.service('addressSrv', function($http, $q) {
-    		this.getCoordinates = function(address) {
-	    		var q = $q.defer();
-	    		var url = 'http://nominatim.openstreetmap.org/search?q=' + encodeURIComponent(address) + '&format=json';
-
-	    		$http.get(url)
-	    			.then(function(data){
-	    				q.resolve(data);
-	    			}, function error(err) {
-	    				q.reject(err);
-	    			});
-	    			
-	    			return q.promise;
-	    		};
-    })
-    
-    .service('zoneSrv', function($http, $q) {
-    		this.getZones = function() {
-			var q = $q.defer();
-			$http.get('http://datasets.antwerpen.be/v4/gis/paparkeertariefzones.json')
-				.then(function(data, status, headers, config){
-					q.resolve(data.data);
-				}, function error(err) {
-					q.reject(err);
-	    			console.log("test");
-				});
-			
-			return q.promise;
-		};
-		
-		// http://alienryderflex.com/polygon/
-		this.inPolygon = function(location, polyLoc){
-			var lastPoint = polyLoc[polyLoc.length-1];
-			var isInside = false;
-			var x = location[0];
-
-			for(var i = 0; i < polyLoc.length; i++){
-				var point = polyLoc[i];
-				var x1 = lastPoint[0];
-				var x2 = point[0];
-				var dx = x2 - x1;
-
-				if(Math.abs(dx) > 180.0){
-					if(x > 0){
-						while(x1 < 0)
-							x1 += 360;
-						while(x2 < 0)
-							x2 += 360;
-					}
-					else{
-						while(x1 > 0)
-							x1 -= 360;
-						while(x2 > 0)
-							x2 -= 360;
-					}
-					dx = x2 - x1;
-				}
-
-				if((x1 <= x && x2 > x) || (x1 >= x && x2 < x)){
-					var grad = (point[1] - lastPoint[1]) / dx;
-					var intersectAtLat = lastPoint[1] + ((x - x1) * grad);
-
-					if(intersectAtLat > location[1])
-						isInside = !isInside;
-				}
-				lastPoint = point;
-			}
-			return isInside;
-		};
-		
-		this.getTariff = function(lng, lat, zones){
-			console.log(zones.length);
-			for(var i = 0; i < zones.length; i++) {
-				console.log(i);
-				var geo = JSON.parse(zones[i].geometry);
-				var coordinates = geo.coordinates[0];
-				if(this.inPolygon([lng, lat], coordinates)) {
-	    			console.log(zones[i].tariefkleur);
-					return zones[i].tariefkleur;
-				}
-			}
-		};
-    })*/
-    
     .service('saveSrv', function($window, $http){
 		  this.setObject = function(key, value){
 			  $window.localStorage[key] = JSON.stringify(value);
